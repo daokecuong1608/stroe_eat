85a1c5bd6f2b046018abb7ab7ff47d2935fd38db
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-10-03 01:54:49 +0700

    ghi log

--- src/main/java/sv/cuong/store_eat/controller/LoginController.java
+++ src/main/java/sv/cuong/store_eat/controller/LoginController.java
@@ -3,6 +3,8 @@ package sv.cuong.store_eat.controller;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,9 +25,19 @@ public class LoginController {
     private LoginService loginService;
     @Autowired
     private JwtUtilHelper jwtUtilHelper;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @PostMapping("/signin")
     public ResponseEntity<?> signin(@RequestParam String username, @RequestParam String password) {
         ResponseData responseData = new ResponseData();
+
+        logger.trace("Kiem tra trace log");
+        logger.debug("Kiem tra debug log");
+        logger.info("Kiem tra info log");
+        logger.warn("Kiem tra warn log");
+        logger.error("Kiem tra error log");
+
         if (loginService.checkLogin(username, password)) {
             String token = jwtUtilHelper.generateToken(username);
             System.out.println(token);
