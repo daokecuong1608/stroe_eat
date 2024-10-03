18fb887933049e84d67aee10e1f9560aaa873a3f
Cương <154745357+daokecuong1608@users.noreply.github.com>
2024-10-04 00:38:29 +0700

    connect -> add information Redis

---
+++ src/main/java/sv/cuong/store_eat/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package sv.cuong.store_eat.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean //ket noi den server
+    public LettuceConnectionFactory redisConnection() {
+        //tao thong tin cau hinh
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName("localhost");
+        configuration.setPort(6380);
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean //set tempplate
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory lettuceConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory);
+        return template;
+    }
+}
+//SD redis co du lieu trk day len redis
+//redis la  1 he thong => cache su luu tru tren o cung SSD
--- src/main/java/sv/cuong/store_eat/service/CatagoryService.java
+++ src/main/java/sv/cuong/store_eat/service/CatagoryService.java
@@ -1,10 +1,13 @@
 package sv.cuong.store_eat.service;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import sv.cuong.store_eat.dto.CatagoryDTO;
 import sv.cuong.store_eat.dto.MenuDTO;
@@ -13,6 +16,7 @@ import sv.cuong.store_eat.entity.Food;
 import sv.cuong.store_eat.repository.CatagoryRepository;
 import sv.cuong.store_eat.service.impl.CatagoryServiceIpml;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,30 +26,47 @@ public class CatagoryService implements CatagoryServiceIpml {
     @Autowired
     private CatagoryRepository catagoryRepository;
 
-    @Cacheable("catagoryhome")
+    // @Cacheable("catagoryhome")
 //luu du lieu lan dau lay vao ram => lan sau goi controller chi vc lay trong ram => ko can truy van
+    @Autowired
+    private RedisTemplate redisTemplate;
 
+
+    private Gson gson = new Gson();
 
     @Override
     public List<CatagoryDTO> getCatagoryHomePage() {
-        PageRequest pageRequest = PageRequest.of(0, 2, Sort.by("id"));
-        Page<Category> result = catagoryRepository.findAll(pageRequest);
-        List<CatagoryDTO> resultList = new ArrayList<>();
-        for (Category category : result) {
-            CatagoryDTO catagoryDTO = new CatagoryDTO();
-            catagoryDTO.setName(category.getNameCate());
-            List<MenuDTO> list = new ArrayList<>();
-            for (Food food : category.getListFood()) {
-                MenuDTO menuDTO = new MenuDTO();
-                menuDTO.setTitle(food.getTitle());
-                menuDTO.setFreeShip(food.isFreeship());
-                menuDTO.setImage(food.getImage());
-                list.add(menuDTO);
-            }
-            catagoryDTO.setMenus(list);
-            resultList.add(catagoryDTO);
-        }
+        List<CatagoryDTO> catagoryDTOs = new ArrayList<>();
 
-        return resultList;
+        //ktra du lieu co tren redis chua
+        String dataRedis = (String) redisTemplate.opsForValue().get("catagory");
+        if (dataRedis != null) {
+            System.out.println("chua co data");
+            PageRequest pageRequest = PageRequest.of(0, 2, Sort.by("id"));
+            Page<Category> listCategory = catagoryRepository.findAll(pageRequest);
+            for (Category category : listCategory) {
+                CatagoryDTO catagoryDTO = new CatagoryDTO();
+                catagoryDTO.setName(category.getNameCate());
+                List<MenuDTO> list = new ArrayList<>();
+                for (Food food : category.getListFood()) {
+                    MenuDTO menuDTO = new MenuDTO();
+                    menuDTO.setTitle(food.getTitle());
+                    menuDTO.setFreeShip(food.isFreeship());
+                    menuDTO.setImage(food.getImage());
+                    list.add(menuDTO);
+                }
+                catagoryDTO.setMenus(list);
+                catagoryDTOs.add(catagoryDTO);
+            }
+            String dataJSON = gson.toJson(catagoryDTOs);
+            //day data len redis de luu tru
+            redisTemplate.opsForValue().set("catagory", dataJSON);
+        } else {
+            Type type = new TypeToken<List<CatagoryDTO>>() {
+            }.getType();
+            catagoryDTOs = gson.fromJson(dataRedis, type);
+            System.out.println("co data base");
+        }
+        return catagoryDTOs;
     }
 }
